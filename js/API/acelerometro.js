3892a915248b83140b96af115c41abe04182c99e
erilke <erilkemx@gmail.com>
2015-11-28 10:43:04 -0600

    ver0

--- js/API/acelerometro.js
+++ js/API/acelerometro.js
@@ -1,5 +1,5 @@
 var ac ={
-	options:{ frequency: 3000 },
+	options:{ frequency: 500 },
 	watchId: null,
 
 	init: function() {
@@ -16,8 +16,7 @@ var ac ={
 
 	$('#valor_acelerometro').html('X: ' + acceleration.x + '\n' +
           ' Y: ' + acceleration.y + '\n' +
-          ' Z: ' + acceleration.z + '\n' +
-          'TS: '      + acceleration.timestamp + '\n');	 	
+          ' Z: ' + acceleration.z + '\n' );	 	
 
 	},
 
