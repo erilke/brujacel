4c978b845b4e8b6d513cdbb1ea15002eb37bae8a
erilke <erilkemx@gmail.com>
2015-11-28 11:06:50 -0600

    ver0

--- js/API/brujula.js
+++ js/API/brujula.js
@@ -5,14 +5,13 @@ var br ={
 	init: function() {
 		alert("Iniciar");
 		if (!br.watchId)
-		var watchID = navigator.compass.watchHeading(br.onSuccess, br.onError, br.options);
+		br.watchID = navigator.compass.watchHeading(br.onSuccess, br.onError, br.options);
 	},
 	stop: function(){
 		alert("Detener");
 		navigator.compass.clearWatch(br.watchID);
 	},
 	onSuccess: function(heading) {
-    //alert('Heading: ' + heading.magneticHeading);
     	$('#valor_brujula').html('Apuntando al: ' + heading.magneticHeading );	
 	},
 
